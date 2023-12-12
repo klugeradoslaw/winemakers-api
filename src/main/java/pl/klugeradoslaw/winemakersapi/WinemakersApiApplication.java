f65543d4202d423065b251d3a578d03e530fd100
Radoslaw Kluge <kluge.radoslaw@gmail.com>
2023-12-12 08:50:10 +0100

    init commit

---
+++ src/main/java/pl/klugeradoslaw/winemakersapi/WinemakersApiApplication.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.winemakersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinemakersApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WinemakersApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/klugeradoslaw/winemakersapi/WinemakersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.klugeradoslaw.winemakersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinemakersApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
